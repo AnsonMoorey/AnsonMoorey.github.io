10e4f51554ecc6c80dd6ea61ed7ef7f5c1eea349
AnsonMoorey <84635480+AnsonMoorey@users.noreply.github.com>
2021-11-30 13:56:50 -0500

    Update send_cmd_message_to_target.bat

--- content/send_cmd_message_to_target.bat
+++ content/send_cmd_message_to_target.bat
@@ -3,4 +3,4 @@ title Send CMD Message to Target
 set /P target=Enter Target PC:
 set /P sec=Enter Display Time (secs):
 msg * /server:%target% /time:%sec%
-set /P closefunction=Press <enter> to close this window
+set /P exit=...End Process...
